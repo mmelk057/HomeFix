0a03ef622835d94665c2855de8bcdf71eaa87712
Maxim Melkonian <mmelk057@uottawa.ca>
2018-10-25 14:48:55 -0400

    Fixed the Bug that did not allow a User to navigate from login to welcome page. Also added firm email validity backend code to ensure that email is not garbage. WelcomePage.xml was modified

--- app/src/main/java/org/homefix/homefix/Login.java
+++ app/src/main/java/org/homefix/homefix/Login.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.regex.Pattern;
+
 public class Login extends AppCompatActivity {
 
     @Override
@@ -38,11 +40,41 @@ public class Login extends AppCompatActivity {
 
                 EditText username = (EditText)findViewById(R.id.editText3);
                 EditText password = (EditText)findViewById(R.id.editText4);
-                if (username.getText().toString().contains("@") && password.getText().length()!=0){ //If username is an email and password isn't null
-                    Intent toWelcome = new Intent(Login.this,Welcome.class);
-                    toWelcome.putExtra("user",username.getText().toString());
-                    startActivity(toWelcome);
 
+                //splitting the username into two fields
+                String email = username.getText().toString();
+                if (email.contains("@") && password.getText().length()!=0){
+                    String [] emailParts = email.split(Pattern.quote("@"));
+                    if (emailParts.length==2) {
+                        boolean areEmailSectionsValid = true;
+                        for(int i=0;i<2;i++){
+                            if(emailParts[i].equals("") || (emailParts[i].startsWith(".")) || (emailParts[i].endsWith("."))){  //an email split by @ cannot have either of its sections start with or end with '.'
+                                areEmailSectionsValid=false;
+                                break;
+                            }
+                            else if(emailParts[i].contains(".")){
+                                String[] emailPartsV2 = emailParts[i].split(Pattern.quote("."));
+                                for (int j = 0; j < emailPartsV2.length; j++) {
+                                    if (emailPartsV2[j].equals("")) {
+                                        areEmailSectionsValid = false;
+                                        break;
+                                    }
+                                }
+
+                            }
+                        }
+                        if(areEmailSectionsValid){
+                            Intent toWelcome = new Intent(Login.this, Welcome.class);
+                            toWelcome.putExtra("user", username.getText().toString());
+                            try {
+                                startActivity(toWelcome);
+                            } catch (Exception e) {
+                                System.out.println("Error Starting Activity: " + e.getMessage() + "\n" + e.getStackTrace());
+
+                            }
+
+                        }
+                    }
                 }
 
             }
