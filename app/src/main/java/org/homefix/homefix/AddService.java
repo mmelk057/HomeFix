643c927991eba424a6b4c353de9b3875dd51e6c2
Brian Kamau <32204151+bk4159@users.noreply.github.com>
2018-11-09 13:27:24 -0500

    added adminservices activity, added addservice activity, added editservice activity

---
+++ app/src/main/java/org/homefix/homefix/AddService.java
@@ -0,0 +1,13 @@
+package org.homefix.homefix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addservice);
+    }
+}
